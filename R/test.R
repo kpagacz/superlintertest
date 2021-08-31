f41cce61575e92d838481ffaeaa502a25b9c182d
kpagacz <konrad.pagacz@gmail.com>
2021-08-31 06:22:00 +0200

    fails

--- R/test.R
+++ R/test.R
@@ -1,6 +1,7 @@
-print("This line spans an eggregiously large portion of the display. Have you considered rewiriting it?")
-
-# This shouldn't fly
-if(TRUE) "test"
-
-# commented_code = "bad"
+#' test
+#' @import shiny
+#'
+test_function <- function() {
+  ns <- NS
+  ns("test")
+}
